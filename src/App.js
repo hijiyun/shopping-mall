43f91b608edd7590950304c4c72e7ff6ccfb46ac
박지윤 <tkrhkfld123@naver.com>
2023-07-18 23:27:05 +0900

    navbar Add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { Button,Navbar,Container,Nav } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" data-bs-theme="light" style={{backgroundColor:"red"}}>
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Button variant="primary">Primary</Button>{' '}
     </div>
   );
 }
