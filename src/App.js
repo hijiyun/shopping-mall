54b4346695bd992fcc3d9829332ffcfb0cf5525b
박지윤 <tkrhkfld123@naver.com>
2023-07-19 23:22:52 +0900

    main background img Add

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { CiSearch } from "react-icons/ci";
 
 function App() {
   return (
+    <>
     <div className="header">
       <Navbar bg="light" data-bs-theme="light" id='nav'>
         <Container id='nav-container'>
           {/* <Navbar.Brand href="#home"></Navbar.Brand> */}
           <Nav className="me-auto">
+            <div className='nav-left'>
             <Nav.Link href="#features">
             <a className='nav-insta' href="https://www.instagram.com/j___y______/">INSTAGRAM</a>
             </Nav.Link>
@@ -23,20 +25,27 @@ function App() {
                 </div>
               </div>
             </Nav.Link>
+            </div>
             <Nav.Link href="#home" className='logo'>
               <a href="#home">
                 <img src="https://cloudemotion.kr/web/mobile/newDesign/0571dd857814e10de79bba8341d1a95f.png" alt="logo" />
               </a>
             </Nav.Link>
+            <div className='nav-right'>
               <Nav.Link href="#features" className='login'>LOGIN</Nav.Link>
               <Nav.Link href="#pricing" className='cart'>CART</Nav.Link>
               <Nav.Link href="#pricing" className='search'>
                 <a href="#" alt="돋보기"><CiSearch color='black' /></a>
               </Nav.Link>
+            </div>
           </Nav>
         </Container>
       </Navbar>
     </div>
+    <div className='main-bg'>
+      
+    </div>
+    </>
   );
 }
 
