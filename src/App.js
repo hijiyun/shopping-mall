fecec66a09455a239dfe75bde41df14f6ad3d880
박지윤 <tkrhkfld123@naver.com>
2023-07-19 21:36:54 +0900

    background-color change

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
           {/* <Navbar.Brand href="#home"></Navbar.Brand> */}
           <Nav className="me-auto">
             <Nav.Link href="#features">
-            <a className='nav-insta' href="https://www.instagram.com/j___y______/">Instagram</a>
+            <a className='nav-insta' href="https://www.instagram.com/j___y______/">INSTAGRAM</a>
             </Nav.Link>
             <Nav.Link href="#">
               <div className='dropdown'>
-                <div className='nav-community'>Community</div>
+                <div className='nav-community'>COMMUNITY</div>
                 <div className='community-content'>
                   <a href="#">notice</a>
                   <a href="#">QnA</a>
@@ -22,13 +22,16 @@ function App() {
                 </div>
               </div>
             </Nav.Link>
-            <Nav.Link href="#home">
+            <Nav.Link href="#home" className='logo'>
               <a href="#home">
                 <img src="https://cloudemotion.kr/web/mobile/newDesign/0571dd857814e10de79bba8341d1a95f.png" alt="logo" />
               </a>
             </Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#features" className='login'>LOGIN</Nav.Link>
+              <Nav.Link href="#pricing" className='cart'>CART</Nav.Link>
+              <Nav.Link href="#pricing" className='search'>
+                <a href="#" alt="돋보기">찾기</a>
+              </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
