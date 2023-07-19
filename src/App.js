1796c38a2c4058a8e726e5ec14614e0b67a2d042
박지윤 <tkrhkfld123@naver.com>
2023-07-19 09:42:41 +0900

    클라우드이모션 Add

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import { Button,Navbar,Container,Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="light" data-bs-theme="light" >
+    <div className="header">
+      <Navbar bg="light" data-bs-theme="light" id='nav'>
         <Container id='nav-container'>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          {/* <Navbar.Brand href="#home"></Navbar.Brand> */}
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#69c037tures">
+            <a href="https://www.instagram.com/j___y______/">instagram</a>
+            </Nav.Link>
+            <Nav.Link href="#home">
+              <a href="#home">
+                <img src="https://cloudemotion.kr/web/mobile/newDesign/0571dd857814e10de79bba8341d1a95f.png" alt="logo" />
+              </a>
+            </Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
