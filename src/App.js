a6fe2fc0fa3fdbb7defe0d968b64ea5b23e56945
박지윤 <tkrhkfld123@naver.com>
2023-07-20 00:03:22 +0900

    items img update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Navbar,Container,Nav } from 'react-bootstrap';
+import { Navbar,Container,Nav,Row,Col } from 'react-bootstrap';
 import { CiSearch } from "react-icons/ci";
 
 
@@ -42,8 +42,31 @@ function App() {
         </Container>
       </Navbar>
     </div>
-    <div className='main-bg'>
-      
+    <div className='main-bg'></div>
+    <div className='main2'>
+    <Container className='items'>
+      <div className='main2-text'>
+        <h2>WEEKLY BEST ITEM</h2>
+        <p class="sub">ღ 클라우드이모션이 사랑받는 아이템 ღ</p>
+      </div>
+      <Row>
+        <Col>
+          <a href="#">
+            <img className='main2-img' src="https://cloudemotion.kr/web/product/medium/202306/1c49cb28b5d836f13873fb1879ce1b0b.webp" alt="사진1" />
+          </a>
+        </Col>
+        <Col>
+          <a href="#">
+            <img className='main2-img' src="https://cloudemotion.kr/web/product/medium/202307/403cb649e88101eb5f00c375e291858a.webp" alt="사진2" />
+          </a>
+        </Col>
+        <Col>
+          <a href="#">
+            <img className='main2-img' src="https://cloudemotion.kr/web/product/medium/202305/d99aaee08c94b35c0cdf9c577de1265e.webp" alt="img3" />
+          </a>
+        </Col>
+      </Row>
+    </Container>
     </div>
     </>
   );
