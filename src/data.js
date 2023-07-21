297b3ed9dd2515abf3fc73c4b77f64f582e33dac
박지윤 <tkrhkfld123@naver.com>
2023-07-21 10:03:41 +0900

    data.js Add

---
+++ src/data.js
@@ -0,0 +1,47 @@
+let data = [
+    {
+        id : 0,
+        title : "[MD pick] 글램핏 데일리 T",
+        price : "27,600원"
+    },
+    {
+        id : 1,
+        title : "[MD pick] 도쿄 in 워싱 T",
+        price : "32,400원"
+    },
+    {
+        id : 2,
+        title : "네스 트위드 점프수트 PT (벨트SET)",
+        price : "39,800원"
+    },
+    {
+        id : 3,
+        title : "[솜다 pick] 비엔나 슈브 롱 랩 SK",
+        price : "32,300원"
+    },
+    {
+        id : 4,
+        title : "[MD pick] 크레디앙 데님 탑 OPS",
+        price : "43,000원"
+    },
+    {
+        id : 5,
+        title : "[솜다 pick] 홀리키 4피스 비키니 SET",
+        price : "36,000원"
+    },
+    {
+        id : 6,
+        title : "[솜다 pick] 울 스킨 백 홀 스트링 T",
+        price : "24,000원"
+    },
+    {
+        id : 7,
+        title : "[솜다 pick] 딥블루 워싱 하이 부츠컷 PT",
+        price : "46,000원"
+    },
+    {
+        id : 8,
+        title : "[ ♡MADE♡ ] tutu off shirring T + 타이 SET (ss.ver)",
+        price : "40,000원"
+    }
+]
